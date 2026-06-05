52eff6e00167ce2b2da1b53a5b5e08b15dd490fa
Guilherme <143668234+correa-dias-gui@users.noreply.github.com>
2026-06-05 15:30:30 -0400

    Feature/dados clinicos (#1)
    * extraindo informação sobre contraindicacoes e advertencias e precauções
    
    * modelo de llm recebe um prompt definido
    
    * gera os prompts para cada rota de api
    
    * testes unitários
    
    * API recebe dados do paciente
    
    ---------
    
    Co-authored-by: alinesahdo <aline.silva@icomp.ufam.edu.br>

--- src/modelo_llm/open_router.py
+++ src/modelo_llm/open_router.py
@@ -1,41 +1,21 @@
 from openrouter import OpenRouter
-from src.processador_texto.processador_texto import get_interacoes
 import os
 
-A = "cefalexina"
-B = "dipirona"
+def llm (content):
+    with OpenRouter(api_key=os.getenv("OPENROUTER_API_KEY")) as client:
+        response = client.chat.send(
+            model = "openai/gpt-oss-120b:free",
+            # max_tokens = 200,
+            messages = [
+                {
+                    "role": "user",
+                    "content": content
+                }
+            ]
 
-content = f"""
-                Use as informações e responda:
-
-                Os medicamentos {A} e {B} possuem interações entre si?
-                
-                Responda apenas "Sim" ou "Não". 
-
-                Informações dos medicamentos:
-                {A}: {get_interacoes(A)}
-
-
-                {B}: {get_interacoes(B)}
-
-                """
-
-print(content)
-
-with OpenRouter(api_key=os.getenv("OPENROUTER_API_KEY")) as client:
-    response = client.chat.send(
-        model = "openai/gpt-oss-120b:free",
-        # max_tokens = 200,
-        messages = [
-            {
-                "role": "user",
-                "content": content
-            }
-        ]
-
-    )
-print(response.model)
-print(response.choices[0].message.content)
+        )
+    
+    return response.choices[0].message.content
 
 
 # response = requests.post(
